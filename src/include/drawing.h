PandTomB/uae4arm

2017-08-06 Beta version with code from WinUAE 3.3.0 and lot of new stuff

src/include/drawing.h
@@ -4,6 +4,11 @@
  * Copyright 1996-1998 Bernd Schmidt
  */
 
+#ifndef UAE_DRAWING_H
+#define UAE_DRAWING_H
+
+#include "uae/types.h"
+
 #define MAX_PLANES 8
 
 /* According to the HRM, pixel data spends a couple of cycles somewhere in the chips
@@ -15,28 +20,29 @@
  * invisible anyway due to hardware DDF limits. */
 #define DISPLAY_LEFT_SHIFT 0x38
 
-#define PIXEL_XPOS(HPOS) (((HPOS)*2 - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1) )
+#define PIXEL_XPOS(HPOS) (((HPOS)*2 - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1) << lores_shift)
 
 #define min_diwlastword (0)
 #define max_diwlastword   (PIXEL_XPOS(0x1d4>> 1))
 
-#define lores_shift 0
+extern int lores_shift, interlace_seen;
 extern bool aga_mode;
 
 STATIC_INLINE int coord_hw_to_window_x (int x)
 {
   x -= DISPLAY_LEFT_SHIFT;
-	return x;
+	return x << lores_shift;
 }
 
 STATIC_INLINE int coord_window_to_hw_x (int x)
 {
+	x >>= lores_shift;
   return x + DISPLAY_LEFT_SHIFT;
 }
 
 STATIC_INLINE int coord_diw_to_window_x (int x)
 {
-	return (x - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1);
+	return (x - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1) << lores_shift;
 }
 
 STATIC_INLINE int coord_window_to_diw_x (int x)
@@ -47,25 +53,47 @@ STATIC_INLINE int coord_window_to_diw_x (int x)
 
 extern int framecnt;
 
-
 /* color values in two formats: 12 (OCS/ECS) or 24 (AGA) bit Amiga RGB (color_regs),
  * and the native color value; both for each Amiga hardware color register. 
  *
  * !!! See color_reg_xxx functions below before touching !!!
  */
+#define CE_BORDERBLANK 0
+#define CE_BORDERSPRITE 2
+#define CE_SHRES_DELAY 4
+
+STATIC_INLINE bool ce_is_borderblank(uae_u8 data)
+{
+	return (data & (1 << CE_BORDERBLANK)) != 0;
+}
+STATIC_INLINE bool ce_is_bordersprite(uae_u8 data)
+{
+	return (data & (1 << CE_BORDERSPRITE)) != 0;
+}
+
 struct color_entry {
   uae_u16 color_regs_ecs[32];
   xcolnr acolors[256];
   uae_u32 color_regs_aga[256];
-	bool borderblank, bordersprite;
+	uae_u8 extra;
 };
 
 /* convert 24 bit AGA Amiga RGB to native color */
-#ifndef PANDORA
-#define CONVERT_RGB(c) \
-    ( xbluecolors[((uae_u8*)(&c))[0]] | xgreencolors[((uae_u8*)(&c))[1]] | xredcolors[((uae_u8*)(&c))[2]] )
-#else
-STATIC_INLINE uae_u16 CONVERT_RGB(uae_u32 c)
+#ifdef ARMV6_ASSEMBLY
+STATIC_INLINE uae_u32 CONVERT_RGB(uae_u32 c)
+{
+  uae_u32 ret;
+  __asm__ (
+			"ubfx    r1, %[c], #19, #5 \n\t"
+			"ubfx    r2, %[c], #10, #6 \n\t"
+			"ubfx    %[v], %[c], #3, #5 \n\t"
+			"orr     %[v], %[v], r1, lsl #11 \n\t"
+			"orr     %[v], %[v], r2, lsl #5 \n\t"
+			"pkhbt   %[v], %[v], %[v], lsl #16 \n\t"
+           : [v] "=r" (ret) : [c] "r" (c) : "r1", "r2" );
+  return ret;
+}
+STATIC_INLINE uae_u16 CONVERT_RGB_16(uae_u32 c)
 {
   uae_u16 ret;
   __asm__ (
@@ -77,6 +105,19 @@ STATIC_INLINE uae_u16 CONVERT_RGB(uae_u32 c)
            : [v] "=r" (ret) : [c] "r" (c) : "r1", "r2" );
   return ret;
 }
+#else
+/* warning: this is still ugly, but now works with either byte order */
+#ifdef WORDS_BIGENDIAN
+# define CONVERT_RGB(c) \
+	( xbluecolors[((uae_u8*)(&c))[3]] | xgreencolors[((uae_u8*)(&c))[2]] | xredcolors[((uae_u8*)(&c))[1]] )
+# define CONVERT_RGB_16(c) \
+	( xbluecolors[((uae_u8*)(&c))[3]] | xgreencolors[((uae_u8*)(&c))[2]] | xredcolors[((uae_u8*)(&c))[1]] )
+#else
+#define CONVERT_RGB(c) \
+    ( xbluecolors[((uae_u8*)(&c))[0]] | xgreencolors[((uae_u8*)(&c))[1]] | xredcolors[((uae_u8*)(&c))[2]] )
+#define CONVERT_RGB_16(c) \
+    ( xbluecolors[((uae_u8*)(&c))[0]] | xgreencolors[((uae_u8*)(&c))[1]] | xredcolors[((uae_u8*)(&c))[2]] )
+#endif
 #endif
 
 STATIC_INLINE xcolnr getxcolor (int c)
@@ -107,7 +148,7 @@ STATIC_INLINE void color_reg_set (struct color_entry *ce, int c, int v)
 /* ugly copy hack, is there better solution? */
 STATIC_INLINE void color_reg_cpy (struct color_entry *dst, struct color_entry *src)
 {
-	dst->borderblank = src->borderblank;
+	dst->extra = src->extra;
   if (aga_mode)
   	/* copy acolors and color_regs_aga */
   	memcpy (dst->acolors, src->acolors, sizeof(struct color_entry) - sizeof(uae_u16) * 32);
@@ -133,11 +174,10 @@ struct color_change {
 };
 
 /* 440 rather than 880, since sprites are always lores.  */
-#define MAX_PIXELS_PER_LINE 880
+#define MAX_PIXELS_PER_LINE 1760
 #define MAX_VIDHEIGHT 270
 
-/* No divisors for MAX_PIXELS_PER_LINE; we support AGA and may one day
-   want to use SHRES sprites.  */
+/* No divisors for MAX_PIXELS_PER_LINE; we support AGA and SHRES sprites  */
 #define MAX_SPR_PIXELS (((MAXVPOS + 1)*2 + 1) * MAX_PIXELS_PER_LINE)
 
 struct sprite_entry
@@ -152,7 +192,6 @@ union sps_union {
   uae_u8 bytes[MAX_SPR_PIXELS];
   uae_u32 words[MAX_SPR_PIXELS / 4];
 };
-
 extern union sps_union spixstate;
 extern uae_u16 spixels[MAX_SPR_PIXELS];
 
@@ -200,14 +239,14 @@ extern int coord_native_to_amiga_y (int);
 extern int coord_native_to_amiga_x (int);
 
 extern void hsync_record_line_state (int lineno);
+extern void partial_draw_frame(void);
+extern void halt_draw_frame(void);
 extern void vsync_handle_redraw (void);
 extern void vsync_handle_check (void);
 extern void init_hardware_for_drawing_frame (void);
 extern void reset_drawing (void);
 extern void drawing_init (void);
-
-extern unsigned long time_per_frame;
-extern void adjust_idletime(unsigned long ns_waited);
+extern bool notice_interlace_seen (bool);
 
 /* Finally, stuff that shouldn't really be shared.  */
 
@@ -225,7 +264,5 @@ STATIC_INLINE void clear_inhibit_frame (int bit)
 {
   inhibit_frame &= ~(1 << bit);
 }
-STATIC_INLINE void toggle_inhibit_frame (int bit)
-{
-  inhibit_frame ^= 1 << bit;
-}
+
+#endif /* UAE_DRAWING_H */

Chips-fr/uae4arm-rpi

src/include/drawing.h
@@ -4,6 +4,11 @@
  * Copyright 1996-1998 Bernd Schmidt
  */
 
+#ifndef UAE_DRAWING_H
+#define UAE_DRAWING_H
+
+#include "uae/types.h"
+
 #define MAX_PLANES 8
 
 /* According to the HRM, pixel data spends a couple of cycles somewhere in the chips
@@ -15,28 +20,29 @@
  * invisible anyway due to hardware DDF limits. */
 #define DISPLAY_LEFT_SHIFT 0x38
 
-#define PIXEL_XPOS(HPOS) (((HPOS)*2 - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1) )
+#define PIXEL_XPOS(HPOS) (((HPOS)*2 - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1) << lores_shift)
 
 #define min_diwlastword (0)
 #define max_diwlastword   (PIXEL_XPOS(0x1d4>> 1))
 
-#define lores_shift 0
+extern int lores_shift, interlace_seen;
 extern bool aga_mode;
 
 STATIC_INLINE int coord_hw_to_window_x (int x)
 {
   x -= DISPLAY_LEFT_SHIFT;
-	return x;
+	return x << lores_shift;
 }
 
 STATIC_INLINE int coord_window_to_hw_x (int x)
 {
+	x >>= lores_shift;
   return x + DISPLAY_LEFT_SHIFT;
 }
 
 STATIC_INLINE int coord_diw_to_window_x (int x)
 {
-	return (x - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1);
+	return (x - DISPLAY_LEFT_SHIFT + DIW_DDF_OFFSET - 1) << lores_shift;
 }
 
 STATIC_INLINE int coord_window_to_diw_x (int x)
@@ -47,26 +53,47 @@ STATIC_INLINE int coord_window_to_diw_x (int x)
 
 extern int framecnt;
 
-
 /* color values in two formats: 12 (OCS/ECS) or 24 (AGA) bit Amiga RGB (color_regs),
  * and the native color value; both for each Amiga hardware color register. 
  *
  * !!! See color_reg_xxx functions below before touching !!!
  */
+#define CE_BORDERBLANK 0
+#define CE_BORDERSPRITE 2
+#define CE_SHRES_DELAY 4
+
+STATIC_INLINE bool ce_is_borderblank(uae_u8 data)
+{
+	return (data & (1 << CE_BORDERBLANK)) != 0;
+}
+STATIC_INLINE bool ce_is_bordersprite(uae_u8 data)
+{
+	return (data & (1 << CE_BORDERSPRITE)) != 0;
+}
+
 struct color_entry {
   uae_u16 color_regs_ecs[32];
   xcolnr acolors[256];
   uae_u32 color_regs_aga[256];
-	bool borderblank, bordersprite;
+	uae_u8 extra;
 };
 
 /* convert 24 bit AGA Amiga RGB to native color */
-//#ifndef PANDORA
-#if !defined(PANDORA) || !defined(USE_ARMNEON)  // Well not really since ubfx is arm6t2...
-#define CONVERT_RGB(c) \
-    ( xbluecolors[((uae_u8*)(&c))[0]] | xgreencolors[((uae_u8*)(&c))[1]] | xredcolors[((uae_u8*)(&c))[2]] )
-#else
-STATIC_INLINE uae_u16 CONVERT_RGB(uae_u32 c)
+#ifdef ARMV6T2
+STATIC_INLINE uae_u32 CONVERT_RGB(uae_u32 c)
+{
+  uae_u32 ret;
+  __asm__ (
+			"ubfx    r1, %[c], #19, #5 \n\t"
+			"ubfx    r2, %[c], #10, #6 \n\t"
+			"ubfx    %[v], %[c], #3, #5 \n\t"
+			"orr     %[v], %[v], r1, lsl #11 \n\t"
+			"orr     %[v], %[v], r2, lsl #5 \n\t"
+			"pkhbt   %[v], %[v], %[v], lsl #16 \n\t"
+           : [v] "=r" (ret) : [c] "r" (c) : "r1", "r2" );
+  return ret;
+}
+STATIC_INLINE uae_u16 CONVERT_RGB_16(uae_u32 c)
 {
   uae_u16 ret;
   __asm__ (
@@ -78,6 +105,19 @@ STATIC_INLINE uae_u16 CONVERT_RGB(uae_u32 c)
            : [v] "=r" (ret) : [c] "r" (c) : "r1", "r2" );
   return ret;
 }
+#else
+/* warning: this is still ugly, but now works with either byte order */
+#ifdef WORDS_BIGENDIAN
+# define CONVERT_RGB(c) \
+	( xbluecolors[((uae_u8*)(&c))[3]] | xgreencolors[((uae_u8*)(&c))[2]] | xredcolors[((uae_u8*)(&c))[1]] )
+# define CONVERT_RGB_16(c) \
+	( xbluecolors[((uae_u8*)(&c))[3]] | xgreencolors[((uae_u8*)(&c))[2]] | xredcolors[((uae_u8*)(&c))[1]] )
+#else
+#define CONVERT_RGB(c) \
+    ( xbluecolors[((uae_u8*)(&c))[0]] | xgreencolors[((uae_u8*)(&c))[1]] | xredcolors[((uae_u8*)(&c))[2]] )
+#define CONVERT_RGB_16(c) \
+    ( xbluecolors[((uae_u8*)(&c))[0]] | xgreencolors[((uae_u8*)(&c))[1]] | xredcolors[((uae_u8*)(&c))[2]] )
+#endif
 #endif
 
 STATIC_INLINE xcolnr getxcolor (int c)
@@ -108,7 +148,7 @@ STATIC_INLINE void color_reg_set (struct color_entry *ce, int c, int v)
 /* ugly copy hack, is there better solution? */
 STATIC_INLINE void color_reg_cpy (struct color_entry *dst, struct color_entry *src)
 {
-	dst->borderblank = src->borderblank;
+	dst->extra = src->extra;
   if (aga_mode)
   	/* copy acolors and color_regs_aga */
   	memcpy (dst->acolors, src->acolors, sizeof(struct color_entry) - sizeof(uae_u16) * 32);
@@ -134,11 +174,10 @@ struct color_change {
 };
 
 /* 440 rather than 880, since sprites are always lores.  */
-#define MAX_PIXELS_PER_LINE 880
+#define MAX_PIXELS_PER_LINE 1760
 #define MAX_VIDHEIGHT 270
 
-/* No divisors for MAX_PIXELS_PER_LINE; we support AGA and may one day
-   want to use SHRES sprites.  */
+/* No divisors for MAX_PIXELS_PER_LINE; we support AGA and SHRES sprites  */
 #define MAX_SPR_PIXELS (((MAXVPOS + 1)*2 + 1) * MAX_PIXELS_PER_LINE)
 
 struct sprite_entry
@@ -153,7 +192,6 @@ union sps_union {
   uae_u8 bytes[MAX_SPR_PIXELS];
   uae_u32 words[MAX_SPR_PIXELS / 4];
 };
-
 extern union sps_union spixstate;
 extern uae_u16 spixels[MAX_SPR_PIXELS];
 
@@ -201,14 +239,14 @@ extern int coord_native_to_amiga_y (int);
 extern int coord_native_to_amiga_x (int);
 
 extern void hsync_record_line_state (int lineno);
+extern void partial_draw_frame(void);
+extern void halt_draw_frame(void);
 extern void vsync_handle_redraw (void);
 extern void vsync_handle_check (void);
 extern void init_hardware_for_drawing_frame (void);
 extern void reset_drawing (void);
 extern void drawing_init (void);
-
-extern long time_per_frame;
-extern void adjust_idletime(long ns_waited);
+extern bool notice_interlace_seen (bool);
 
 /* Finally, stuff that shouldn't really be shared.  */
 
@@ -226,7 +264,5 @@ STATIC_INLINE void clear_inhibit_frame (int bit)
 {
   inhibit_frame &= ~(1 << bit);
 }
-STATIC_INLINE void toggle_inhibit_frame (int bit)
-{
-  inhibit_frame ^= 1 << bit;
-}
+
+#endif /* UAE_DRAWING_H */